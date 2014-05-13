c76f19dd6fe9c759cfcb561dfba743128280444f
Filipe Nevola <filipe.nevola@tecsinapse.com.br>
2014-05-13 19:35:19 -0300

    ajustes gerais

--- src/main/java/br/com/tecsinapse/camel/cdi/EnvProperties.java
+++ src/main/java/br/com/tecsinapse/camel/cdi/EnvProperties.java
@@ -6,15 +6,6 @@ import org.aeonbits.owner.Config.Sources;
 @Sources({"classpath:env.properties"})
 public interface EnvProperties extends Config {
 
-	@Key("host")
-	String host();
-
-	@Key("env")
-	String env();
-
-    @Key("branch")
-    String branch();
-
     @Key("camel.route.twitter")
     boolean camelRouteTwitter();
 
